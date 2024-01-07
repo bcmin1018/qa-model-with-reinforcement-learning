97fd708ec8876a8e44b2fad6303974132dbd2e36
bcmin <bcmin1018@gmail.com>
2024-01-07 14:31:57 +0900

    Model merge 최종

--- RL/model_merge.py
+++ RL/model_merge.py
@@ -1,13 +1,12 @@
-from peft import PeftConfig, PeftModel
+from peft import PeftModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-access_token="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"
-base_model_name = "EleutherAI/polyglot-ko-1.3b"
-adapter_model_name = "bradmin/sft_adapter"
-output_name = "bradmin/sft"
+access_token=""
+base_model_name = ""
+adapter_model_name = ""
+output_name = ""
 
 def model_merge(base_model_name, adapter_model_name, output_name):
-  peft_config = PeftConfig.from_pretrained(adapter_model_name)
 
   model = AutoModelForCausalLM.from_pretrained(
       base_model_name
