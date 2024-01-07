9322bf3b293f87ec0ab78ba77586fd91ccb1aab1
bcmin <bcmin1018@gmail.com>
2024-01-07 14:34:26 +0900

    코드 정리

--- crawling/database/Mongodb_connect.py
+++ crawling/database/Mongodb_connect.py
@@ -1,11 +1,7 @@
 import pymongo
 import os
-# from crawling.util.JsonConfigFileManager import  JsonConfigFileManager
-from util.JsonConfigFileManager import JsonConfigFileManager
+from crawling.util.JsonConfigFileManager import JsonConfigFileManager
 from pathlib import Path
-
-
-# https://www.bearpooh.com/171
 
 root_dir = Path(os.path.dirname(os.path.abspath(__file__))).parent
 
@@ -30,7 +26,6 @@ class MongodbConnection:
 
     def insert_many(self, collection, data):
         self.db.get_collection(collection).insert_many(data)
-        # self.db[collection].insert_many(data)
 
     def find(self, collection=None, filter=None, projection=None, skip=0, limit=0, sort=None, return_key=False):
         coll = self.db.get_collection(collection)
@@ -54,17 +49,6 @@ class MongodbConnection:
         result = self.db.get_collection(collection).aggregate([
     {"$group": {"_id": "$questionId", "question": {"$first": "$question"}, "answer": {"$first": "$answer"}, "code": {"$first": "$code"}, "url": {"$first": "$url"}}}
 ])
-        # if projection is None:
-        #     result = self.db.get_collection(collection).distinct(key)
-        # else:
-        #     result = self.db.get_collection(collection).distinct(key, projection)
-        # distinct_col = self.db.get_collection(collection).distinct(key)
-        # if key is None:
-        #     query = {f'{key}' : {'$in': distinct_col}}
-        #     result = self.db.get_collection(collection).find(query)
-        # else:
-        #     query = {f'{key}' : {'$in': distinct_col}}
-        #     result = self.db.get_collection(collection).find(query, projection)
         return result
 
     def close(self):
@@ -73,17 +57,3 @@ class MongodbConnection:
     def __del__(self):
         if self.con is not None:
             self.close()
-
-
-
-
-
-
-# conn = pymongo.MongoClient("mongodb://{}:{}@{}:{}/{}".format('chatbot', 'funnywork1018!', '54.180.9.189', int('27017'), 'healthcare_chatbot'))
-# db = conn.get_database('healthcare_chatbot')
-# db.list_collection_names()
-# print(db.list_collection_names())
-# col = db['hidoc']
-# mydict = [{ "mb_name": "homzzang", "mb_level": "10" }]
-# x = col.insert_many(mydict)
-# print(x)
